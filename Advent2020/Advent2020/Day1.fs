600947d8b5291fb9bde594d0b89567def32dbd56
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-01 19:27:13 +0100

    Day 1 Naive n^3 implementation, but at least it works

--- Advent2020/Advent2020/Day1.fs
+++ Advent2020/Advent2020/Day1.fs
@@ -1,5 +1,30 @@
 ﻿namespace Advent2020
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+module Day1 =
+    let findMatch sum sequence e =
+        let sumIs sum s =
+            if e + s = sum then Some (e, s) else None
+        Seq.choose (sumIs sum) sequence |> Seq.tryHead
+        
+    let findMatch3 sum sequence e =
+        Seq.choose (findMatch (sum - e) sequence) sequence
+        |> Seq.tryHead
+        |> Option.map (fun (x, y) -> (e, x, y))
+        
+        
+        
+    let part1 (numbers: string) =
+        let sequence = numbers.Split('\n') |> Array.map (int) |> Array.toSeq
+        let found = Seq.choose (findMatch 2020 sequence) sequence |> Seq.tryHead
+        match found with
+            | Some ( a, b ) -> a * b
+            | _ -> 0
+            
+    let part2 (numbers: string) =
+        let sequence = numbers.Split('\n') |> Array.map (int) |> Array.toSeq
+        let found = Seq.choose (findMatch3 2020 sequence) sequence |> Seq.tryHead
+        match found with
+            | Some ( a, b, c ) -> a * b * c
+            | _ -> 0
+        
