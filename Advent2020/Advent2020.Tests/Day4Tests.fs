5504b8f4c8a0d8753fda099cbbe5c66190f068cb
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-04 13:40:48 +0100

    Part 4 day 1

---
+++ Advent2020/Advent2020.Tests/Day4Tests.fs
@@ -0,0 +1,29 @@
+﻿module Advent2020.Tests.Day4Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day4
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in"
+    let result = part1 input
+    Assert.Equal(2, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day4.txt")
+    let result = part1 numbers
+    Assert.Equal(226, result)
---
+++ Advent2020/Advent2020/Day4.fs
@@ -0,0 +1,25 @@
+﻿namespace Advent2020
+
+open System
+open System.Collections.Generic
+open FParsec
+
+module Day4 =
+    let key = many letter |>> String.Concat
+    let value = many (letter <|> digit <|> pchar '#') |>> String.Concat
+    let kvp = (key .>> pchar ':' .>>. value) .>> optional (pchar ' ' <|> pchar '\n')
+    let passport = (many1 (kvp)) .>> spaces |>> dict
+    let parsec input =
+        match run (many passport) input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+        
+    let valid (d: IDictionary<string, string>) =
+        ["byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid"]
+        |> List.map d.ContainsKey
+        |> List.fold (&&) true
+    let part1 input =
+        parsec input
+        |> List.filter valid
+        |> List.length
+
