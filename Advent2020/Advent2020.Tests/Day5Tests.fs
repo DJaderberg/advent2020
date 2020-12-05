6c8d832260ee50d27acabf34997c7cd55bd3f269
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-05 06:43:48 +0100

    Day 5 initial solution
    Part 2 is limited in range by hand, which is probably not general enough, but it's what I used to actually solve the puzzle

---
+++ Advent2020/Advent2020.Tests/Day5Tests.fs
@@ -0,0 +1,24 @@
+﻿module Advent2020.Tests.Day5Tests
+
+open FParsec
+open System.IO
+open Xunit
+open Advent2020.Day5
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "BFFFBBFRRR"
+    let result = part1 input
+    Assert.Equal(567, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day5.txt")
+    let result = part1 numbers
+    Assert.Equal(987, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day5.txt")
+    let result = part2 numbers
+    Assert.Equal(603, result)
---
+++ Advent2020/Advent2020/Day5.fs
@@ -0,0 +1,35 @@
+﻿namespace Advent2020
+
+open System
+open System.Linq
+open System.Collections.Generic
+open FParsec
+
+module Day5 =
+    let cToB c = match c with | 'B' -> 1 | 'F' -> 0 | _ -> -1
+    let rToB c = match c with | 'R' -> 1 | 'L' -> 0 | _ -> -1
+    let rec arrToId exp sum arr =
+        let h = List.head arr
+        match exp with
+        | 1 -> sum + h*exp
+        | _ -> arrToId (exp/2) (sum+h*exp) (List.tail arr)
+    let row = manyMinMaxSatisfy 7 7 (fun c -> c = 'F' || c = 'B') |>> (Seq.toList >> List.map cToB >> (arrToId 64 0))
+    let col = manyMinMaxSatisfy 3 3 (fun c -> c = 'R' || c = 'L') |>> (Seq.toList >> List.map rToB >> (arrToId 4 0))
+    let id = row .>>. col |>> (fun (r, c) -> r * 8 + c)
+
+    let idList = sepEndBy id spaces1
+    let parsec input =
+        match run idList input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+        
+    let part1 input =
+        parsec input
+        |> List.max
+        
+    let part2 input =
+        let set =
+            parsec input
+            |> Set.ofList
+        Seq.find (fun seat -> not (Set.contains seat set)) (seq { 96 .. 897 })
+
