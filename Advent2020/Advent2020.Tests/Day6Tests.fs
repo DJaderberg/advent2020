639db54b67d11a5f743486c4f6684775955e4181
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-06 08:51:17 +0100

    Day 6

--- Advent2020/Advent2020.Tests/Day6Tests.fs
+++ Advent2020/Advent2020.Tests/Day6Tests.fs
@@ -2,29 +2,57 @@
 
 open System.IO
 open Xunit
-open Advent2020.Day5
+open Advent2020.Day6
 
 [<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
+    let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"
     let result = part1 input
-    Assert.Equal(567, result)
+    Assert.Equal(11, result)
     
 [<Fact>]
 let ``Part 1`` () =
     let numbers = File.ReadAllText("Data/day6.txt")
     let result = part1 numbers
-    Assert.Equal(987, result)
+    Assert.Equal(6630, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
-    let input = ""
+    let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"
     let result = part2 input
-    Assert.Equal(567, result)
+    Assert.Equal(6, result)
     
 [<Fact>]
 let ``Part 2`` () =
     let numbers = File.ReadAllText("Data/day6.txt")
     let result = part2 numbers
-    Assert.Equal(603, result)
+    Assert.Equal(3437, result)
 
--- Advent2020/Advent2020/Day6.fs
+++ Advent2020/Advent2020/Day6.fs
@@ -4,8 +4,24 @@ open System
 open FParsec
 
 module Day6 =
-    let part1 input = 2
+    let person = many1 letter
+    let group = sepEndBy1 person newline 
+    let groupCountAnyone = group |>> List.concat |>> set |>> Set.count
+    
+    let allYes list =
+        let allToFind = List.concat list |> set |> Set.toList
+        List.filter (fun c -> List.forall (List.contains c) list) allToFind
+    let groupCountEveryone = group |>> allYes |>> List.length
+    
+    let totalCountAnyone = sepEndBy1 groupCountAnyone newline |>> List.sum
+    let totalCountEveryone = sepEndBy1 groupCountEveryone newline |>> List.sum
+    
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+    let part1 input = parsec totalCountAnyone input
         
-    let part2 input = 2
+    let part2 input = parsec totalCountEveryone input
 
 
