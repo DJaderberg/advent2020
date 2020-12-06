61489d19f3d4af4b25777fdcc14be88691cfe661
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-06 16:39:27 +0100

    Prep for day 7

---
+++ Advent2020/Advent2020.Tests/Day7Tests.fs
@@ -0,0 +1,30 @@
+﻿module Advent2020.Tests.Day7Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day7
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = ""
+    let result = part1 input
+    Assert.Equal(42, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day7.txt")
+    let result = part1 numbers
+    Assert.Equal(42, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = ""
+    let result = part2 input
+    Assert.Equal(42, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day7.txt")
+    let result = part2 numbers
+    Assert.Equal(42, result)
+
---
+++ Advent2020/Advent2020/Day7.fs
@@ -0,0 +1,12 @@
+﻿namespace Advent2020
+
+open System
+open FParsec
+
+module Day7 =
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+    let part1 input = parsec (letter |>> int) input
+    let part2 input = raise (NotImplementedException())
