053076635178d8c52ac10eb86be17dc694719ae8
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-06 19:07:32 +0100

    Use set operations for day 6
    Makes for a nice solution

--- Advent2020/Advent2020/Day6.fs
+++ Advent2020/Advent2020/Day6.fs
@@ -4,24 +4,16 @@ open System
 open FParsec
 
 module Day6 =
-    let person = many1 letter
-    let group = sepEndBy1 person newline 
-    let groupCountAnyone = group |>> List.concat |>> set |>> Set.count
-    
-    let allYes list =
-        let allToFind = List.concat list |> set |> Set.toList
-        List.filter (fun c -> List.forall (List.contains c) list) allToFind
-    let groupCountEveryone = group |>> allYes |>> List.length
-    
-    let totalCountAnyone = sepEndBy1 groupCountAnyone newline |>> List.sum
-    let totalCountEveryone = sepEndBy1 groupCountEveryone newline |>> List.sum
-    
+    let person = many1 letter |>> set
+    let group op = sepEndBy1 person newline |>> op
+    let groupCount op = group op |>> Set.count
+    let totalCount op = sepEndBy1 (groupCount op) newline |>> List.sum
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
-    let part1 input = parsec totalCountAnyone input
+    let part1 input = parsec (totalCount Set.unionMany) input
         
-    let part2 input = parsec totalCountEveryone input
+    let part2 input = parsec (totalCount Set.intersectMany) input
 
 
