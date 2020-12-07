0f9d373f873ecc95d42c3ac4227f2a124bf2198c
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-07 18:03:34 +0100

    Day 7 part 1

--- Advent2020/Advent2020.Tests/Day7Tests.fs
+++ Advent2020/Advent2020.Tests/Day7Tests.fs
@@ -5,22 +5,36 @@ open Xunit
 open Advent2020.Day7
 
 [<Fact>]
+let ``Part 1 Example single`` () =
+    let input = "light red bags contain 1 bright white bag, 2 muted yellow bags."
+    let result = parsec rule input
+    result
+    
+[<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
+    let input = "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(4, result)
     
 [<Fact>]
 let ``Part 1`` () =
     let numbers = File.ReadAllText("Data/day7.txt")
     let result = part1 numbers
-    Assert.Equal(42, result)
+    Assert.Equal(4, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
     let input = ""
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(4, result)
     
 [<Fact>]
 let ``Part 2`` () =
--- Advent2020/Advent2020/Day7.fs
+++ Advent2020/Advent2020/Day7.fs
@@ -1,12 +1,50 @@
 ﻿namespace Advent2020
 
 open System
+open System.Collections
+open System.Collections.Generic
 open FParsec
 
 module Day7 =
+    type Tree = Node of string * Tree | Leaf of string
+    let color =
+        ((many letter .>> spaces1 |>> String.Concat) .>>. (many letter .>> spaces1 |>> String.Concat)
+        |>> fun (x, y) -> x + y)
+        .>> (pstring "bags" <|> pstring "bag")
+    let bag = (many1 digit |>> String.Concat |>> int) .>> spaces1 .>>. color
+    let flip (x, y) = (y, x)
+    let contains = sepEndBy1 bag (pchar ',' >>. spaces1) |>> List.map flip |>> dict
+    let none = pstring "no other bags" |>> (fun _ -> dict [])
+    let rule = (color .>> (pstring " contain ")) .>>. (none <|> contains) .>> pchar '.'
+    let rules = sepEndBy1 rule newline |>> dict
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
-    let part1 input = parsec (letter |>> int) input
+        
+    let rec traverseGraph (graphElements: IDictionary<string, seq<string>>) matches queue =
+        match List.tryHead queue with
+        | Some element ->
+            if graphElements.ContainsKey element then
+                let newMatches = (graphElements.Item element |> Set.ofSeq) - Set.union matches (Set.ofList queue)
+                let newQueue = List.append (List.tail queue) (Set.toList newMatches)
+                traverseGraph graphElements (Set.union matches newMatches) newQueue
+            else
+                traverseGraph graphElements (Set.add element matches) (List.tail queue)
+        | None -> matches
+        
+    let matchSet input =
+        let parsed = parsec rules input
+        let containsMap = parsed |> Seq.map (fun kvp -> (kvp.Key, kvp.Value.Keys |> Seq.cast<string>))
+        let inverted =
+            containsMap
+            |> Seq.map (fun (v, sequence) -> Seq.map (fun e -> (v, e)) sequence)
+            |> Seq.concat
+            |> Seq.map flip
+            |> Seq.groupBy fst
+            |> Seq.map (fun (k, sequenceTup) -> (k, Seq.map snd sequenceTup))
+            |> dict
+        traverseGraph inverted Set.empty (List.singleton "shinygold")
+    let part1 input = matchSet input |> Set.count
+        
     let part2 input = raise (NotImplementedException())
