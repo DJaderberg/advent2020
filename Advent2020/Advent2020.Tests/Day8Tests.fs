a4dab4416b9eb2b6a7c4760481b5e6011e196261
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-08 06:24:03 +0100

    Day 8 part 1

---
+++ Advent2020/Advent2020.Tests/Day8Tests.fs
@@ -0,0 +1,45 @@
+﻿module Advent2020.Tests.Day8Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day8
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"
+    let result = part1 input
+    Assert.Equal(5, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day8.txt")
+    let result = part1 numbers
+    Assert.Equal(2058, result)
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"
+    let result = part1 input
+    Assert.Equal(8, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day8.txt")
+    let result = part1 numbers
+    Assert.Equal(2058, result)
+
---
+++ Advent2020/Advent2020/Day8.fs
@@ -0,0 +1,34 @@
+﻿namespace Advent2020
+
+open System
+open System.Collections
+open System.Collections.Generic
+open FParsec
+
+module Day8 =
+    type Instruction = Jmp of int | Nop of int | Acc of int
+    let jmp = pstring "jmp " >>. pint32 |>> Jmp
+    let nop = pstring "nop " >>. pint32 |>> Nop
+    let acc = pstring "acc " >>. pint32 |>> Acc
+    
+    let instr = acc <|> jmp <|> nop
+    let program = sepEndBy1 instr newline |>> Array.ofList
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+        
+    let rec exec (program: Instruction[]) (alreadyExeced: Set<int>) acc pc =
+        if Set.contains pc alreadyExeced then
+            acc
+        else
+            match Array.get program pc with
+            | Nop _ -> exec program (Set.add pc alreadyExeced) acc (pc+ 1)
+            | Jmp i -> exec program (Set.add pc alreadyExeced) acc (pc + i)
+            | Acc i -> exec program (Set.add pc alreadyExeced) (acc + i) (pc+ 1)
+    let part1 input =
+        let prog = parsec program input
+        exec prog Set.empty 0 0
+        
+    let part2 input = raise (NotImplementedException())
+
