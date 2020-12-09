3c3001bd51a0cf3e98840995dfcc4264e8057e00
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-09 07:30:09 +0100

    Day 9

--- Advent2020/Advent2020.Tests/Day9Tests.fs
+++ Advent2020/Advent2020.Tests/Day9Tests.fs
@@ -6,20 +6,62 @@ open Advent2020.Day9
 
 [<Fact>]
 let ``Part 1 Example`` () =
-    let input = "nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6"
-    let result = part1 input
-    Assert.Equal(5, result)
+    let input = "35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"
+    let result = part1 5L input
+    Assert.Equal(127L, result)
     
 [<Fact>]
 let ``Part 1`` () =
     let numbers = File.ReadAllText("Data/day9.txt")
-    let result = part1 numbers
-    Assert.Equal(2058, result)
+    let result = part1 25L numbers
+    Assert.Equal(1212510616L, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = "35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"
+    let result = part2 5L input
+    Assert.Equal(62L, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day9.txt")
+    let result = part2 25L numbers
+    Assert.Equal(171265123L, result)
--- Advent2020/Advent2020/Day9.fs
+++ Advent2020/Advent2020/Day9.fs
@@ -3,13 +3,56 @@
 open System
 open System.Collections
 open System.Collections.Generic
+open System.Diagnostics.CodeAnalysis
 open FParsec
 
 module Day9 =
+    let number = many1 digit |>> String.Concat |>> int64
+    let numbers = sepEndBy1 number newline
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
         
-    let part1 input = parsec parser input
+    let hasMatch (numbers: seq<int64 * int64>) (sum: int64) =
+        let elem e e2 = fst e + fst e2 = sum && snd e <> snd e2
+        let subSearch e = (Seq.filter (elem e) numbers) |> Seq.tryHead
+        let m =
+            numbers
+            |> Seq.choose subSearch
+            |> Seq.tryHead
+        m |> Option.map (fun _ -> sum)
+        
+    let rec findNonMatch (preambleSize: int64) (ql: int64) (queue: Queue<int64 * int64>) (numbers: seq<int64 * int64>) =
+        if ql = preambleSize then
+            let next = Seq.head numbers
+            match hasMatch queue (fst next) with
+            | Some _ -> 
+                let _ = queue.Dequeue()
+                let _ = queue.Enqueue(next)
+                findNonMatch preambleSize ql queue (Seq.tail numbers)
+            | None -> fst next
+        else
+            let next = Seq.head numbers
+            let _ = queue.Enqueue(next)
+            findNonMatch preambleSize (ql + 1L) queue (Seq.tail numbers)
+    let part1 (size: int64) input =
+        let ns = parsec numbers input
+        findNonMatch size 0L (Queue<int64 * int64>()) (Seq.zip (Seq.ofList ns) (seq { 0L..1000000L }))
+        
+    let rec contiguousSum sum (options: (int64 * int64) list) numbers =
+        let h = List.head numbers
+        let opts = List.map (fun e -> (fst h + fst e, snd e)) options |> List.filter (fun e -> fst e <= sum)
+        let m = opts |> List.filter (fun e -> fst e = sum) |> List.tryHead
+        match m with
+        | Some (v, start) -> (start , snd h)
+        | None -> contiguousSum sum (List.head numbers :: opts) (List.tail numbers)
+    let part2 (size: int64) input =
+        let ns = parsec numbers input
+        let sum = findNonMatch size 0L (Queue<int64 * int64>()) (Seq.zip (Seq.ofList ns) (seq { 0L..1000000L }))
+        let (l, u) = contiguousSum sum List.empty (Seq.zip ns (seq { 0L .. 1000000L }) |> List.ofSeq)
+        let range = ns |> List.skip (int l) |> List.take (int (u - l + 1L))
+        let min = List.min range
+        let max = List.max range
+        min + max
         
