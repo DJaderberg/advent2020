686a0f17bd22db2dc5289b68b9e584eaf7a77a8a
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-10 18:35:26 +0100

    Prep for day 11

---
+++ Advent2020/Advent2020.Tests/Day11Tests.fs
@@ -0,0 +1,31 @@
+﻿module Advent2020.Tests.Day11Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day11
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = ""
+    let result = part1 input
+    Assert.Equal(42, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day11.txt")
+    let result = part1 numbers
+    Assert.Equal(42, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = ""
+    let result = part2 input
+    Assert.Equal(42, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day11.txt")
+    let result = part2 numbers
+    Assert.Equal(42, result)
+
+
---
+++ Advent2020/Advent2020/Day11.fs
@@ -0,0 +1,16 @@
+﻿namespace Advent2020
+
+open System
+open System.Collections
+open System.Collections.Generic
+open FParsec
+
+module Day11 =
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+    
+    let part1 input = parsec (many letter |>> String.Concat |>> int) input
+        
+    let part2 input = parsec (many letter |>> String.Concat |>> int) input
