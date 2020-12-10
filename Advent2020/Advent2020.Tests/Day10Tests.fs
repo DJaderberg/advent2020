0d0c10142fd83ac3c6e421424a569afdebcf1b97
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-10 07:07:27 +0100

    Day 10!
    I had an interesting solution method for part 2, it went something like this:
    1. Ask Wolfram alpha for the prime factorization of 19208 (the more complicated example given).
    It's 2^3 * 7^4 = 19208, btw.
    2. Just kind of guess the solution that would imply multiplication by 2 or 7 (turn out you need 2^2=4 also!) :D

--- Advent2020/Advent2020.Tests/Day10Tests.fs
+++ Advent2020/Advent2020.Tests/Day10Tests.fs
@@ -6,25 +6,117 @@ open Advent2020.Day10
 
 [<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
+    let input = "16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4"
     let result = part1 input
-    Assert.Equal(127, result)
+    Assert.Equal(7L * 5L, result)
+    
+[<Fact>]
+let ``Part 1 Example 2`` () =
+    let input = "28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"
+    let result = part1 input
+    Assert.Equal(22L * 10L, result)
     
 [<Fact>]
 let ``Part 1`` () =
-    let numbers = File.ReadAllText("Data/day9.txt")
+    let numbers = File.ReadAllText("Data/day10.txt")
     let result = part1 numbers
-    Assert.Equal(1212510616, result)
+    Assert.Equal(2080L, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
-    let input = ""
+    let input = "16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4"
     let result = part2 input
-    Assert.Equal(62, result)
+    Assert.Equal(8L, result)
+    
+[<Fact>]
+let ``Part 2 Example 2`` () =
+    let input = "28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"
+    let result = part2 input
+    Assert.Equal(19208L, result)
     
 [<Fact>]
 let ``Part 2`` () =
-    let numbers = File.ReadAllText("Data/day9.txt")
+    let numbers = File.ReadAllText("Data/day10.txt")
     let result = part2 numbers
-    Assert.Equal(171265123, result)
+    Assert.Equal(6908379398144L, result)
 
--- Advent2020/Advent2020/Day10.fs
+++ Advent2020/Advent2020/Day10.fs
@@ -7,10 +7,42 @@ open System.Diagnostics.CodeAnalysis
 open FParsec
 
 module Day10 =
+    let adapterJoltage = many1 digit |>> String.Concat |>> int64
+    let adapters = sepEndBy1 adapterJoltage newline
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
-    let part1 input = parsec (pstring "42" |>> int) input
+    
+    let rec differences prev jolts =
+        match jolts with
+        | [] -> []
+        | j :: js -> (j - prev) :: differences j js
         
-    let part2 input = parsec (pstring "42" |>> int) input
+    let part1 input =
+        let orig = parsec adapters input
+        let sorted = List.sort orig
+        let max = List.max sorted
+        let full = List.append sorted [max + 3L]
+        let diffs =
+            differences 0L full
+            |> List.groupBy id |> dict
+        (diffs.[1L].Length |> int64) * (diffs.[3L].Length |> int64)
+        
+    let rec paths curr diffs =
+        match diffs with
+        | [] -> curr
+        | [ _ ] -> curr
+        | 1L :: 1L :: 1L :: 1L :: ds -> paths (7L * curr) ds
+        | 1L :: 1L :: 1L :: ds -> paths (4L * curr) ds
+        | 1L :: 1L :: ds -> paths (2L * curr) ds
+        | d :: ds -> paths curr ds
+        
+    let part2 input =
+        let orig = parsec adapters input
+        let sorted = List.sort orig
+        let max = List.max sorted
+        let full = List.append sorted [max + 3L]
+        let diffs =
+            differences 0L full
+        paths 1L diffs
