d74c0c0328ea2edba925fa47a4e3b0226123c103
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-11 08:33:19 +0100

    Day 11
    Part 2 took an extra long time because I put an accidental line break in my test data which meant that it gave an answer that was just a bit too low...

--- Advent2020/Advent2020.Tests/Day11Tests.fs
+++ Advent2020/Advent2020.Tests/Day11Tests.fs
@@ -6,26 +6,43 @@ open Advent2020.Day11
 
 [<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
+    let input = "L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(37, result)
     
 [<Fact>]
 let ``Part 1`` () =
     let numbers = File.ReadAllText("Data/day11.txt")
     let result = part1 numbers
-    Assert.Equal(42, result)
+    Assert.Equal(2368, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
-    let input = ""
+    let input = "L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(26, result)
     
 [<Fact>]
 let ``Part 2`` () =
     let numbers = File.ReadAllText("Data/day11.txt")
     let result = part2 numbers
-    Assert.Equal(42, result)
-
+    Assert.Equal(2124, result)
 
--- Advent2020/Advent2020/Day11.fs
+++ Advent2020/Advent2020/Day11.fs
@@ -6,11 +6,86 @@ open System.Collections.Generic
 open FParsec
 
 module Day11 =
+    type Spot = Floor | Empty | Occupied
+    let floor = pchar '.' |>> fun _ -> Floor
+    let empty = pchar 'L' |>> fun _ -> Empty
+    let occupied = pchar '#' |>> fun _ -> Occupied
+    let row = many1 (floor <|> empty <|> occupied)
+    let layout = sepEndBy1 row newline |>> array2D
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
     
-    let part1 input = parsec (many letter |>> String.Concat |>> int) input
+    let newState limit curr neighbors =
+        match (curr, neighbors) with
+        | (Empty, 0) -> Occupied
+        | (Occupied, n) when n >= limit -> Empty
+        | (c, _) -> c
+    let eval spot =
+        match spot with
+        | Occupied -> 1
+        | _ -> 0
         
-    let part2 input = parsec (many letter |>> String.Concat |>> int) input
+    let tryGet field (i,j) =
+        let iMax = (Array2D.length1 field)
+        let jMax = (Array2D.length2 field)
+        if i >= iMax || j >= jMax || i < 0 || j < 0 then
+            None
+        else
+            Some (Array2D.get field i j)
+            
+    let update1 field i j e =
+        [(i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1); (i - 1, j - 1); (i + 1, j - 1); (i + 1, j + 1); (i - 1, j + 1)]
+        |> List.choose (tryGet field)
+        |> List.map eval |> List.sum
+        |> newState 4 e
+    
+    let makeStep (is, js) (ci, cj) = (ci + is, cj + js)
+    let rec anyOccupied field step curr =
+        let n = makeStep step curr
+        match tryGet field n with
+        | None -> false
+        | Some Occupied -> true
+        | Some Empty -> false
+        | Some _ -> anyOccupied field step n
+    let update2 field i j e =
+        [(1, 0); (-1, 0); (0, 1); (0, -1); (-1, -1); (1, -1); (1, 1); (-1, 1)]
+        |> List.filter (fun s -> anyOccupied field s (i, j))
+        |> List.length
+        |> newState 5 e
+    
+    let calculate field =
+        let mutable counter = 0
+        let _ = Array2D.iter (fun e -> (counter <- counter + eval e)) field
+        counter
+        
+    let step updater field =
+        let n = field |> Array2D.mapi (updater field)
+        n
+    let rec play updater old =
+        let newLayout = step updater old
+        if newLayout = old then
+            calculate newLayout
+        else
+            play updater newLayout
+            
+    let part1 input =
+        let layout = parsec layout input
+        play update1 layout
+        
+    let part2 input =
+        let layout = parsec layout input
+        play update2 layout
+        
+    let iter2 input n =
+        let layout = parsec layout input
+        seq { 1 .. n }
+        |> Seq.fold (fun s _ -> step update2 s) layout 
+
+    let flatten (A:'a[,]) = A |> Seq.cast<'a>
+    let toString layout =
+        layout
+        |> Array2D.map (function |Occupied -> "#" |Empty -> "L" |Floor -> ".")
+        |> flatten
+        |> Seq.fold (fun s e -> s + e) ""
