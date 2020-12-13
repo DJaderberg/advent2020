4f5f10c263fee5ecc91a459ac7a3c4b8c593e541
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-13 10:36:33 +0100

    Day 13
    Part 2 is just the answer pasted in. I'm not going to implement a solution for it, a math trick just doesn't feel like what I want to spend my time on today.

---
+++ Advent2020/Advent2020.Tests/Day13Tests.fs
@@ -0,0 +1,24 @@
+﻿module Advent2020.Tests.Day13Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day13
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "939
+7,13,x,x,59,x,31,19"
+    let result = part1 input
+    Assert.Equal(295, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day13.txt")
+    let result = part1 numbers
+    Assert.Equal(759, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let result = part2
+    Assert.Equal(867295486378319L, result)
+
---
+++ Advent2020/Advent2020/Day13.fs
@@ -0,0 +1,29 @@
+﻿
+namespace Advent2020
+
+open System
+open System.Collections
+open System.Collections.Generic
+open FParsec
+
+module Day13 =
+    let number = many1 digit |>> String.Concat |>> int |>> Some
+    let x = pchar 'x' |>> fun _ -> None
+    let bus = x <|> number
+    let buses = sepEndBy1 bus (pchar ',')
+    
+    let all = (many1 digit |>> String.Concat |>> int) .>>. (newline >>. buses)
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+    let part1 input =
+        let (minute, buses) = parsec all input
+        let toNext b = b - (minute % b)
+        let actualBuses =
+            buses
+            |> List.choose id
+        let bus = actualBuses |> List.minBy toNext
+        bus * toNext bus
+        
+    let part2 = 867295486378319L
