e3dfac279a6333d7b9e63575acde8932db880bd0
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-14 07:58:02 +0100

    Day 14 part 1

---
+++ Advent2020/Advent2020.Tests/Day14Tests.fs
@@ -0,0 +1,32 @@
+﻿module Advent2020.Tests.Day14Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day14
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0"
+    let result = part1 input
+    Assert.Equal(165UL, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day14.txt")
+    let result = part1 numbers
+    Assert.Equal(6317049172545UL, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = ""
+    let result = part2 input
+    Assert.Equal(295, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day14.txt")
+    let result = part2 numbers
+    Assert.Equal(86729, result)
---
+++ Advent2020/Advent2020/Day14.fs
@@ -0,0 +1,53 @@
+﻿
+
+namespace Advent2020
+
+open System
+open System.Collections
+open System.Collections.Generic
+open FParsec
+
+module Day14 =
+    type Instruction = | Mask of uint64 * uint64 | Mem of int * uint64
+    let rec makeMask defV state bits =
+        match bits with
+        | [] -> state
+        | b :: bs -> makeMask defV ((state <<< 1) + Option.defaultValue defV b) bs
+    let makeOr = makeMask 0UL 0UL
+    let makeAnd = makeMask 1UL 0UL
+        
+    let mask =
+        pstring "mask = "
+        >>. many1 (anyOf ['X'; '1'; '0'])
+        |>> (fun l ->
+            (List.map (function | '0' -> Some 0UL | _ -> None) l,
+             List.map (function | '1' -> Some 1UL | _ -> None) l))
+        |>> (fun (a, b) ->
+            (makeAnd a, makeOr b))
+    let mem = pstring "mem[" >>. (many1 digit |>> String.Concat |>> int)  .>> pstring "] = " .>>. (many1 digit |>> String.Concat |>> uint64)
+    let instruction = (mask |>> Mask) <|> (mem |>> Mem)
+    let list = sepEndBy1 instruction newline
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
+        
+    let apply (a, o) value =
+        (value &&& a) ||| o
+        
+    let rec execInstr mask mem instructions =
+        match instructions with
+        | [] -> mem
+        | i :: is ->
+            match i with
+            | Mask (a, o) -> execInstr (a, o) mem is
+            | Mem (loc, value) ->
+                execInstr mask (Map.add loc (apply mask value) mem) is
+    let part1 input =
+        let instructions = parsec list input
+        let e = execInstr (0UL, UInt64.MaxValue) Map.empty instructions
+        Map.toSeq e
+        |> Seq.map snd
+        |> Seq.sum
+        
+    let part2 input = 86729
