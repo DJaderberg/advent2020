a47687e40969ebc2de48b6d97760d24976905741
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-15 08:11:47 +0100

    Day 15 part 1

--- Advent2020/Advent2020.Tests/Day15Tests.fs
+++ Advent2020/Advent2020.Tests/Day15Tests.fs
@@ -5,15 +5,51 @@ open Xunit
 open Advent2020.Day15
 
 [<Fact>]
-let ``Part 1 Example`` () =
-    let input = ""
-    let result = part1 input
-    Assert.Equal(165, result)
+let ``Part 1 Example 2020`` () =
+    let input = "0,3,6"
+    let result = part1 2020 input
+    Assert.Equal(436, result)
+    
+[<Fact>]
+let ``Part 1 Example 10`` () =
+    let input = "0,3,6"
+    let result = part1 10 input
+    Assert.Equal(0, result)
+    
+[<Fact>]
+let ``Part 1 Example 2`` () =
+    let input = "1,3,2"
+    let result = part1 2020 input
+    Assert.Equal(1, result)
+    
+[<Fact>]
+let ``Part 1 Example 3`` () =
+    let input = "1,2,3"
+    let result = part1 2020 input
+    Assert.Equal(27, result)
+    
+[<Fact>]
+let ``Part 1 Example 4`` () =
+    let input = "2,3,1"
+    let result = part1 2020 input
+    Assert.Equal(78, result)
+    
+[<Fact>]
+let ``Part 1 Example 5`` () =
+    let input = "3,2,1"
+    let result = part1 2020 input
+    Assert.Equal(438, result)
+    
+[<Fact>]
+let ``Part 1 Example 6`` () =
+    let input = "3,1,2"
+    let result = part1 2020 input
+    Assert.Equal(1836, result)
     
 [<Fact>]
 let ``Part 1`` () =
-    let numbers = File.ReadAllText("Data/day14.txt")
-    let result = part1 numbers
+    let numbers = "16,11,15,0,1,7"
+    let result = part1 2020 numbers
     Assert.Equal(42, result)
     
 [<Fact>]
@@ -24,6 +60,6 @@ let ``Part 2 Example`` () =
     
 [<Fact>]
 let ``Part 2`` () =
-    let numbers = File.ReadAllText("Data/day14.txt")
+    let numbers = ""
     let result = part2 numbers
     Assert.Equal(42, result)
--- Advent2020/Advent2020/Day15.fs
+++ Advent2020/Advent2020/Day15.fs
@@ -6,12 +6,32 @@ open System.Collections.Generic
 open FParsec
 
 module Day15 =
+    let number = many1 digit |>> String.Concat |>> int
+    let startingNumbers = sepEndBy1 number (pchar ',')
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
                 
-    let part1 input = 42
+    let rec step (whenSaid, curTurn, prevSaid) remaining =
+        match remaining with
+        | 0 -> prevSaid
+        | _ ->
+            match Map.tryFind prevSaid whenSaid with
+            | None -> step ((Map.add prevSaid curTurn whenSaid), (curTurn + 1), 0) (remaining - 1)
+            | Some turn ->
+                let say = curTurn - turn
+                step ((Map.add prevSaid curTurn whenSaid), (curTurn + 1), say) (remaining - 1)
+    
+    let setup (map, prev, index) current =
+        match prev with
+        | None -> (map, Some current, index + 1)
+        | Some p -> ((Map.add p index map), Some current, index + 1)
+    let part1 turns input =
+        let start = parsec startingNumbers input
+        let (startingMap, prev, setupTurns) = List.fold setup (Map.empty, None, 0) start
+        let p = Option.defaultValue 0 prev
+        step (startingMap, setupTurns, p) (turns - setupTurns)
         
     let part2 input = 42
 
