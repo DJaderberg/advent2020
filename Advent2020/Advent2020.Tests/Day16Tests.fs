e712b55290a8b0dc8da7e05a26e8583b7f8b7a2a
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-16 19:10:53 +0100

    Day 16 part 1
    But also with some beginning changes for part 2 because I forgot to commit at quite the right moment

--- Advent2020/Advent2020.Tests/Day16Tests.fs
+++ Advent2020/Advent2020.Tests/Day16Tests.fs
@@ -6,15 +6,26 @@ open Advent2020.Day16
 
 [<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
+    let input = "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"
     let result = part1 input
-    Assert.Equal(436, result)
+    Assert.Equal(71, result)
     
 [<Fact>]
 let ``Part 1`` () =
-    let numbers = ""
+    let numbers = File.ReadAllText("Data/day16.txt")
     let result = part1 numbers
-    Assert.Equal(662, result)
+    Assert.Equal(19060, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
@@ -24,7 +35,7 @@ let ``Part 2 Example`` () =
     
 [<Fact>]
 let ``Part 2`` () =
-    let numbers = ""
+    let numbers = File.ReadAllText("Data/day16.txt")
     let result = part2 numbers
     Assert.Equal(37312, result)
 
--- Advent2020/Advent2020/Day16.fs
+++ Advent2020/Advent2020/Day16.fs
@@ -6,9 +6,44 @@ open System.Collections.Generic
 open FParsec
 
 module Day16 =
+    type Rule = Rule of string * (int * int) * (int * int)
+    let range = manyChars digit |>> int .>> pchar '-' .>>. (manyChars digit |>> int)
+    let curry3 f a b c = f (a,b,c)
+    let rule =  
+        pipe3
+            (sepEndBy1 (manyChars letter) spaces1 |>> List.fold ((+)) "" .>> pstring ": ")
+            (range .>> pstring " or ")
+            range
+            (curry3 Rule)
+    
+    let values = sepEndBy1 (manyChars digit |>> int) (pchar ',')
+    let yours = (many1 (anyOf (Seq.toList "your ticket:")) >>. newline) >>. values
+    let nearby = (many1 (anyOf (Seq.toList "nearby tickets:")) >>. newline) >>. sepEndBy1 values newline
+    
+    let total =
+        pipe3
+            (sepEndBy1 (attempt rule) newline)
+            (newline >>. yours .>> newline)
+            (newline >>. nearby .>> optional newline)
+            (curry3 id)
+            
+            
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
-        | Failure(e, _, _) -> raise (Exception("Parsing error: " + e))
-    let part1 input = 42
-    let part2 input = 42
+        | Failure(e, _, _) -> raise (Exception(e))
+    
+    let setupRules (array: bool array) rule =
+        match rule with
+        | Rule (_, (a,b), (c,d)) ->
+            for i in Seq.append (seq { a .. b }) (seq {c .. d}) do
+                array.[i] <- true
+            array
+        
+    let isOk (rulesArray : bool array) e = not rulesArray.[e]
+    let part1 input =
+        let (rules, _, nearby) = parsec total input
+        let rulesArray = List.fold setupRules (Array.zeroCreate 1000) rules
+        List.concat nearby |> List.filter (isOk rulesArray) |> List.sum
+    let part2 input =
+        42
