87ade7d8d3b3f675a3bbdf3ea9db12a3307c3a13
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-17 08:35:15 +0100

    Day 17 part 1

--- Advent2020/Advent2020.Tests/Day17Tests.fs
+++ Advent2020/Advent2020.Tests/Day17Tests.fs
@@ -5,22 +5,33 @@ open Xunit
 open Advent2020.Day17
 
 [<Fact>]
+let ``Part 1 Example 1`` () =
+    let input = ".#.
+..#
+###"
+    let result = part1 1 input
+    Assert.Equal(11, result)
+[<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
-    let result = part1 input
-    Assert.Equal(71, result)
+    let input = ".#.
+..#
+###"
+    let result = part1 6 input
+    Assert.Equal(112, result)
     
 [<Fact>]
 let ``Part 1`` () =
     let numbers = File.ReadAllText("Data/day17.txt")
-    let result = part1 numbers
+    let result = part1 6 numbers
     Assert.Equal(19060, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
-    let input = ""
+    let input = ".#.
+..#
+###"
     let result = part2 input
-    Assert.Equal(1, result)
+    Assert.Equal(848, result)
     
 [<Fact>]
 let ``Part 2`` () =
--- Advent2020/Advent2020/Day17.fs
+++ Advent2020/Advent2020/Day17.fs
@@ -6,9 +6,84 @@ open System.Collections.Generic
 open FParsec
 
 module Day17 =
+    let active = pchar '#'
+    let inactive = pchar '.'
+    let p1dSlice = many1 (active <|> inactive)
+    let p2dSlice = sepEndBy1 p1dSlice newline
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception(e))
-    let part1 input = 42
+    let initialLine (x,y, set) next =
+        match next with
+        | '#' -> (x+1, y, Set.add (x,y,0) set)
+        | _ -> (x+1, y, set)
+    let initialGrid (y,set) nexts =
+        let (_,_,newSet) = List.fold initialLine (0, y, set) nexts
+        (y + 1, Set.union set newSet)
+    let isActive set vec = Set.contains vec set
+    let neighbors (x,y,z) =
+        [
+            (x+1,y,z);
+            (x+1,y,z+1);
+            (x+1,y,z-1);
+            (x+1,y+1,z);
+            (x+1,y+1,z+1);
+            (x+1,y+1,z-1);
+            (x+1,y-1,z);
+            (x+1,y-1,z+1);
+            (x+1,y-1,z-1);
+            //(x,y,z);
+            (x,y,z+1);
+            (x,y,z-1);
+            (x,y+1,z);
+            (x,y+1,z+1);
+            (x,y+1,z-1);
+            (x,y-1,z);
+            (x,y-1,z+1);
+            (x,y-1,z-1);
+            (x-1,y,z);
+            (x-1,y,z+1);
+            (x-1,y,z-1);
+            (x-1,y+1,z);
+            (x-1,y+1,z+1);
+            (x-1,y+1,z-1);
+            (x-1,y-1,z);
+            (x-1,y-1,z+1);
+            (x-1,y-1,z-1);
+        ]
+        
+    let stepSingle set vec =
+        let ns = neighbors vec |> Set.ofList |> Set.toList
+        let count = ns |> List.filter (isActive set) |> List.length
+        match count with
+        | 3 -> true
+        | 2 when isActive set vec -> true
+        | _ -> false
+    
+    let updateSingle oldSet set vec =
+        let currently = isActive oldSet vec
+        let next = stepSingle oldSet vec
+        if next then Set.add vec set
+        else set
+    let searchSpace (set: Set<int * int * int>) =
+        let (minX, minY, minZ) =List.reduce (fun (sx: int, sy: int, sz: int) (x,y,z) ->
+            (Math.Min(sx,x), Math.Min(sy,y), Math.Min(sz,z))) (Set.toList set)
+        let (maxX, maxY, maxZ) =List.reduce (fun (sx: int, sy: int, sz: int) (x,y,z) ->
+            (Math.Max(sx,x), Math.Max(sy,y), Math.Max(sz,z))) (Set.toList set)
+        (seq {minX - 1 .. maxX + 1}, seq {minY - 1 .. maxY + 1}, seq {minZ - 1 .. maxZ + 1})
+    let step set =
+        let (spaceX, spaceY, spaceZ) = searchSpace set
+        let range = Seq.allPairs (Seq.allPairs spaceX spaceY) spaceZ |> Seq.map (fun ((x,y),z) -> (x,y,z))
+        Seq.fold (updateSingle set) Set.empty range
+    let rec steps n set =
+        match n with
+        | 0 -> set
+        | _ -> steps (n - 1) (step set)
+    let part1 iterations input =
+        let parsed = parsec p2dSlice input
+        let (_, initial) = List.fold initialGrid (0, Set.empty) parsed
+        let result = steps iterations initial
+        Set.count result
+        
     let part2 input = 42
