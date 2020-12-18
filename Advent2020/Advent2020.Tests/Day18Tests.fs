ad993ef235e022beee88d6ac04a63b86cb30e6f9
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-19 00:10:09 +0100

    Finally done with day 18 part 1.
    Biggest problem was trying to implement a recursive parser with FParsec.
    The trick was to use createParserForwardedToRef

--- Advent2020/Advent2020.Tests/Day18Tests.fs
+++ Advent2020/Advent2020.Tests/Day18Tests.fs
@@ -6,24 +6,54 @@ open Advent2020.Day18
 
 [<Fact>]
 let ``Part 1 Example`` () =
-    let input = ""
+    let input = "3 + 8"
     let result = part1 input
-    Assert.Equal(112, result)
+    Assert.Equal(11L, result)
+    
+[<Fact>]
+let ``Part 1 Example 2`` () =
+    let input = "3 * 8"
+    let result = part1 input
+    Assert.Equal(24L, result)
+    
+[<Fact>]
+let ``Part 1 Example 3`` () =
+    let input = "3 * (8)"
+    let result = part1 input
+    Assert.Equal(24L, result)
+    
+[<Fact>]
+let ``Part 1 Example 4`` () =
+    let input = "3 * 8 + 1"
+    let result = part1 input
+    Assert.Equal(25L, result)
+    
+[<Fact>]
+let ``Part 1 Example 5`` () =
+    let input = "3 * (8 + 1)"
+    let result = part1 input
+    Assert.Equal(27L, result)
+    
+[<Fact>]
+let ``Part 1 Example 6`` () =
+    let input = "(6 + 9 + 2 * 7) + ((6 + 3 + 9) + 5) + (6 * 7 * 7 + (2 * 4 + 2 * 8 * 5 + 3) * (3 + 3 + 6) + 9)"
+    let result = part1 input
+    Assert.Equal(42L, result)
     
 [<Fact>]
 let ``Part 1`` () =
     let numbers = File.ReadAllText("Data/day18.txt")
     let result = part1 numbers
-    Assert.Equal(426, result)
+    Assert.Equal(426L, result)
     
 [<Fact>]
 let ``Part 2 Example`` () =
     let input = ""
     let result = part2 input
-    Assert.Equal(848, result)
+    Assert.Equal(848L, result)
     
 [<Fact>]
 let ``Part 2`` () =
     let numbers = File.ReadAllText("Data/day18.txt")
     let result = part2 numbers
-    Assert.Equal(1892, result)
+    Assert.Equal(1892L, result)
--- Advent2020/Advent2020/Day18.fs
+++ Advent2020/Advent2020/Day18.fs
@@ -1,13 +1,31 @@
 ﻿namespace Advent2020
 
 open System
+open System.Linq.Expressions
+open System.Reflection.Emit
 open FParsec
 
 module Day18 =
+    let term = many1Chars digit |>> int64
+    let operator = (pchar '*' |>> fun _ -> (*)) <|> (pchar '+' |>> fun _ -> (+))
+    let expr, exprImpl = createParserForwardedToRef()
+    let rec parens =
+        pchar '(' >>. spaces >>. expressions .>> spaces .>> pchar ')'
+    and rest = many ((spaces >>. operator) .>>. (spaces >>. expr)) |>> List.map (fun (op, v) -> op v)
+    and expressions =
+        spaces >>. expr .>>. rest
+        |>> (fun (i, fs) -> List.fold (fun s f -> f s) i fs)
+    
+    do exprImpl := choice [parens; term]
+    
     let parsec parser input =
         match run parser input with
         | Success(v, _, _) -> v
         | Failure(e, _, _) -> raise (Exception(e))
         
-    let part1 input = 42
-    let part2 input = 42
+    let part1 (input: string) =
+        let lines = input.Split("\r\n") |> Array.toList
+        let y = lines |> List.map (parsec expressions)
+        y
+        |> List.sum
+    let part2 input = 42L
