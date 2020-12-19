c972039fc1acd4fb4f9e7f7df7359a2d2830e8b4
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-19 17:18:12 +0100

    Day 19 part 1

---
+++ Advent2020/Advent2020.Tests/Day19Tests.fs
@@ -0,0 +1,37 @@
+﻿module Advent2020.Tests.Day19Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day19
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "0: 1 2
+1: \"a\"
+2: 1 3 | 3 1
+3: \"b\"
+
+aab
+a
+ab
+ba"
+    let result = part1 input
+    Assert.Equal(1, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day19.txt")
+    let result = part1 numbers
+    Assert.Equal(279, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = ""
+    let result = part2 input
+    Assert.Equal(51, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day19.txt")
+    let result = part2 numbers
+    Assert.Equal(1892, result)
---
+++ Advent2020/Advent2020/Day19.fs
@@ -0,0 +1,55 @@
+﻿namespace Advent2020
+
+open System
+open System.Linq.Expressions
+open System.Reflection.Emit
+open FParsec
+
+module Day19 =
+    type Rule =
+        | SubRules of int list list
+        | CharRule of char
+    
+    let okChar = anyOf ['a'; 'b']
+    let space = optional (pchar ' ')
+    let id = many1Chars digit |>> int
+    let subrule = space >>. sepEndBy1 id space
+    let subrules = sepEndBy1 subrule (pchar '|') |>> SubRules
+    let charrule = pchar '"' >>. okChar .>> pchar '"' |>> CharRule
+    let rule = many1Chars digit |>> int .>> pchar ':' .>> space .>>. (charrule <|> subrules)
+    let rules = sepEndBy1 rule newline |>> Map.ofList
+    let data = sepEndBy1 (many1 okChar) newline
+    
+    let all = rules .>> newline .>>. data
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception(e))
+        
+    let evalCharRule c (data: char list list): char list list =
+        let evalSingle (d: char list) =
+            match d with
+            | ch :: tail when ch = c -> Some tail
+            | _ -> None
+        List.choose evalSingle data
+    let rec eval (rules: Map<int,Rule>) rule (data: char list list): char list list =
+        match rule with
+        | CharRule c -> evalCharRule c data
+        | SubRules sr -> List.map (evalSubRule rules data) sr |> List.concat
+    and evalSubRule (rules: Map<int, Rule>) (data: char list list) (remainingRules: int list): char list list =
+        match remainingRules with
+        | [] -> data
+        | i :: tail ->
+            if data = [] then [] else
+            let r = Map.find i rules
+            let value = eval rules r data
+            evalSubRule rules value tail
+        
+        
+    let part1 (input: string) =
+        let (rules, data) = parsec all input
+        let result = List.map (fun d -> eval rules (Map.find 0 rules) [d]) data |> List.filter (List.contains [])
+        result
+        |> List.length
+        
+    let part2 (input: string) = 42
