e0347fcc9f3ee9c5533efd8336aca982cc11a83f
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-19 19:10:50 +0100

    Prep for day 20

---
+++ Advent2020/Advent2020.Tests/Day20Tests.fs
@@ -0,0 +1,30 @@
+﻿module Advent2020.Tests.Day20Tests
+
+open System.IO
+open Xunit
+open Advent2020.Day20
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = ""
+    let result = part1 input
+    Assert.Equal(1, result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let numbers = File.ReadAllText("Data/day20.txt")
+    let result = part1 numbers
+    Assert.Equal(279, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = ""
+    let result = part2 input
+    Assert.Equal(12, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let numbers = File.ReadAllText("Data/day20.txt")
+    let result = part2 numbers
+    Assert.Equal(384, result)
+
---
+++ Advent2020/Advent2020/Day20.fs
@@ -0,0 +1,16 @@
+﻿namespace Advent2020
+
+open System
+open System.Linq.Expressions
+open System.Reflection.Emit
+open FParsec
+
+module Day20 =
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception(e))
+        
+    let part1 (input: string) = 42
+        
+    let part2 (input: string) = 42
