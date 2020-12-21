e7c26158e328a0b2d2a5d956e0e990911868f47c
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-21 17:15:36 +0100

    Prep for day 22

---
+++ Advent2020/Advent2020/Day22.fs
@@ -0,0 +1,13 @@
+﻿namespace Advent2020
+
+open System
+open FParsec
+
+module Day22 =
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception(e))
+    let part1 (input: string) = 42
+    let part2 (input: string) = 42
+        
