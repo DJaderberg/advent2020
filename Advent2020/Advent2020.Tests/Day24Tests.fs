72b2170e4cf1f0d620fda62e148626fbe3e56631
David Jäderberg <DJaderberg@users.noreply.github.com>
2020-12-24 08:51:31 +0100

    Day 24 part 1

---
+++ Advent2020/Advent2020.Tests/Day24Tests.fs
@@ -0,0 +1,55 @@
+﻿module Advent2020.Tests.Day24Tests
+
+
+open System.IO
+open Xunit
+open Advent2020.Day24
+
+[<Fact>]
+let ``Part 1 Example`` () =
+    let input = "sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"
+    let result = part1 input
+    Assert.Equal(10, result)
+    
+[<Fact>]
+let ``Part 1 example flip origo`` () =
+    let input = "nwwswee"
+    let result = part1Map input |> Map.toSeq
+    Assert.Equal([(0,0), Black], result)
+    
+[<Fact>]
+let ``Part 1`` () =
+    let input = File.ReadAllText("Data/day24.txt")
+    let result = part1 input
+    Assert.Equal(0, result)
+    
+[<Fact>]
+let ``Part 2 Example`` () =
+    let input = ""
+    let result = part2 input
+    Assert.Equal(0, result)
+    
+[<Fact>]
+let ``Part 2`` () =
+    let input = File.ReadAllText("Data/day24.txt")
+    let result = part2 input
+    Assert.Equal(0, result)
---
+++ Advent2020/Advent2020/Day24.fs
@@ -0,0 +1,47 @@
+﻿namespace Advent2020
+
+open System
+open FParsec
+
+module Day24 =
+    let parsec parser input =
+        match run parser input with
+        | Success(v, _, _) -> v
+        | Failure(e, _, _) -> raise (Exception(e))
+
+    type Color = Black | White
+    type Tile = Tile of string
+    let tile = (pstring "ne" <|> pstring "nw" <|> pstring "sw" <|> pstring "se" <|> pstring "e" <|> pstring "w") |>> Tile
+    let line = many1 tile
+    let total = sepEndBy1 line newline
+    
+    let flipTile d t =
+        match Map.tryFind t d with
+        | None -> Map.add t Black d
+        | Some color -> Map.add t (if color = Black then White else Black) d
+    
+    let step (x,y) tile =
+        match tile with
+        | Tile t ->
+            match t with
+            | "ne" -> (x + 1, y + 1)
+            | "nw" -> (x - 1, y + 1)
+            | "se" -> (x + 1, y - 1)
+            | "sw" -> (x - 1, y - 1)
+            | "e" -> (x + 2, y)
+            | "w" -> (x - 2, y)
+            | _ -> raise (Exception("Unknown direction in step"))
+    let folder map tiles =
+        let position = List.fold step (0,0) tiles
+        flipTile map position
+        
+    let part1Map input =
+        let parsed = parsec total input
+        List.fold folder Map.empty parsed
+    let part1 input =
+        part1Map input
+        |> Map.toList
+        |> List.map snd
+        |> List.filter ((=) Black)
+        |> List.length
+    let part2 input = 42
